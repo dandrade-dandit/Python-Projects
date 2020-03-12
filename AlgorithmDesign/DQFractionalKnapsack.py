61e4e9f5edc2a78cff67a0daffaf534a88d48dac
denisan2875 <33a653847d7c63919d169321eec4a0ae6806ddad>
2020-03-12 14:41:24 -0700

    Commit 01

--- AlgorithmDesign/DQFractionalKnapsack.py
+++ AlgorithmDesign/DQFractionalKnapsack.py
@@ -1,8 +1,8 @@
 def fractionalKnapsack(wt, val, w):
-    wt_val = dict((k, val[k] / wt[k]) for k in range(0, len(wt)))
+    val_wt = dict((k, val[k] / wt[k]) for k in range(0, len(wt)))
     r = 0
 
-    for key, value in sorted(wt_val.items(), key=lambda item: item[1], reverse=True):
+    for key, value in sorted(val_wt.items(), key=lambda item: item[1], reverse=True):
         if w >= wt[key]:
             r += val[key]
             w -= wt[key]
@@ -17,7 +17,9 @@ def fractionalKnapsack(wt, val, w):
 
 # driver code to test the above code
 if __name__ == '__main__':
-    wt = [1, 3, 5, 4, 1, 3, 2, 6]
     val = [5, 10, 15, 7, 8, 9, 4, 20]
+    wt = [1, 3, 5, 4, 1, 3, 2, 6]
+    #val_wt = [5, 3.3, 3, 1.75, 8, 3, 2, 3.33]
+
     w = 15
     print("The result of the Divide and Conquer - Fractional Knapsack problem is: " + str(fractionalKnapsack(wt, val, w)))
